1c5c0a41f82623d7201b4010d9de0582ea1029f5
Adepejustephen <adepejuifeoluwa97@gmail.com>
2022-11-16 13:21:00 +0100

    re-import place images and styled

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { RatingStar } from "../../assests/svgs";
+import { Heart, RatingStar } from "../../assests/svgs";
 
 export const PlaceCard = ({img}) => {
   return (
-    <div className="bg-plCardBg flex flex-col p-4 gap-4 rounded-[0.9375rem] border border-[#D7D7D7] ">
-      <div className=" relative h-[16.25rem]  rounded-[0.9375rem] border border-[#D7D7D7]">
+    <div className="bg-plCardBg flex flex-col p-4 gap-4 rounded-[0.9375rem] border border-[#D7D7D7]  cursor-pointer">
+      <div className=" relative h-[16.25rem]  rounded-[0.9375rem] ">
         <img
           src={img}
           alt="place_card_img"
-          className="object-contain w-full h-full"
+          className="rounded-[0.9375rem] object-cover w-full h-full border border-[#D7D7D7] "
         />
-        
+        <Heart className="absolute top-2 right-3 heart" />
       </div>
       <div className="flex flex-col gap-[0.625rem]">
         <div className="flex justify-between text-txtDark text-xs">
